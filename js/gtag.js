242c27a951c423347d94441ff9c662245c47281e
Vincenzo Argese <vincenzo.argese@gmail.com>
2025-07-29 21:54:55 +0200

    Cookie banner

---
+++ js/gtag.js
@@ -0,0 +1,10 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-XHH0DSS4T3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-XHH0DSS4T3');
+</script>
+  
